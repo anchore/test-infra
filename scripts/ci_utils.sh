2d1a0897adaa12ad311dc3a085780ab03a59f338
Brady Todhunter <bradyt@anchore.com>
2019-04-16 17:12:34 -0700

    clean up trigger vars, add more logging
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- scripts/ci_utils.sh
+++ scripts/ci_utils.sh
@@ -21,7 +21,9 @@ function gather_artifacts {
         VAR="${i//-/_}_SHA"
         touch artifacts.txt
         if [[ "$i" != "$circle_run_repo" ]]; then
+            set -x
             COMMIT_SHA=$(git ls-remote git@github.com:anchore/${i} "refs/heads/${CIRCLE_BRANCH:-master}" | awk '{ print $1 }')
+            set +x
             echo "export $VAR=$COMMIT_SHA" | tee -a artifacts.txt
         elif [[ "$i" = "$circle_run_repo" ]]; then
             echo "export ${VAR^^}=$CIRCLE_SHA1" | tee -a artifacts.txt
@@ -33,11 +35,12 @@ function trigger_artifact_build {
     declare circle_run_repo=$1
     gather_artifacts $circle_run_repo
     source artifacts.txt
+    set -eux
     curl --user ${CIRCLE_API_TOKEN}: \
          --data build_parameters[CIRCLE_JOB]=build \
          --data build_parameters[anchore_engine_SHA]=${anchore_engine_SHA} \
          --data build_parameters[anchore_cli_SHA]=${anchore_cli_SHA} \
          --data build_parameters[enterprise_SHA]=${enterprise_SHA} \
          --data build_parameters[anchore_on_prem_ui_SHA]=${anchore_on_prem_ui_SHA} \
-    https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/tree/${CIRCLE_BRANCH}
+    https://circleci.com/api/v1.1/project/github/anchore/release-candidates/tree/${CIRCLE_BRANCH}
 }
